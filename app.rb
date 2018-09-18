b7c80ad1edff7c0dceb678ee99a1badda1f2cb25
Vu Le <core.safecrlf>
2018-09-18 17:26:58 +0100

    pass capybara feature test for updating screen with players names

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ class Battle < Sinatra::Base
     erb(:index)
   end
 
+  post '/players_info' do
+    @player1_name = params[:player1_name]
+    @player2_name = params[:player2_name]
+    erb(:players_info)
+  end
+
   run! if app_file == $0
 end
 
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,4 +4,5 @@ feature 'Welcome Battle' do
     visit('/')
     expect(page).to have_content 'Welcome to Battle!'
   end
+  
 end
